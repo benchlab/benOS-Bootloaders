16153a14151dd1c97a0283e964363fbfb54b3549
Jared Rice Sr <39349252+JaredRiceSr@users.noreply.github.com>
2018-05-18 22:52:13 -0500

    Removed tests from notes and added to bootloader-test library.

--- print.asm
+++ print.asm
@@ -10,26 +10,19 @@
 ; found in Rust's Redox. Pieces from Ubuntu's boot loading functions
 ; were also used in the benOS bootloader's library of parts as well as many
 ; other bootloaders created over the years.
+;
+; CREDIT: Canonical in Ubuntu 16.04. Translated to RUST for benOS
 ; ==================================================================
 
 SECTION .text
 USE16
-; provide function for printing in x86 real mode
 
-; print a string and a newline
-; CLOBBER
-;   ax
 print_line:
     mov al, 13
     call print_char
     mov al, 10
     jmp print_char
 
-; print a string
-; IN
-;   si: points at zero-terminated String
-; CLOBBER
-;   si, ax
 print:
     pushf
     cld
@@ -43,9 +36,6 @@ print:
     popf
     ret
 
-; print a character
-; IN
-;   al: character to print
 print_char:
     pusha
     mov bx, 7
@@ -53,14 +43,10 @@ print_char:
     int 0x10
     popa
     ret
-
-; print a number in hex
-; IN
-;   bx: the number
-; CLOBBER
-;   al, cx
+    
 print_hex:
     mov cx, 4
+
 .lp:
     mov al, bh
     shr al, 4
@@ -69,6 +55,7 @@ print_hex:
     jb .below_0xA
 
     add al, 'A' - 0xA - '0'
+
 .below_0xA:
     add al, '0'
 
