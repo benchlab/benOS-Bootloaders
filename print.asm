9a6e346a740fb6c7d0cbc139936ddfcad9db1225
Jared Rice Sr <39349252+JaredRiceSr@users.noreply.github.com>
2018-05-18 21:57:24 -0500

    added print_stage to the benOS Bootloader

---
+++ print.asm
@@ -0,0 +1,80 @@
+; ------------------------------------------------------------------
+; benOS Initial 
+; ------------------------------------------------------------------
+
+; ==================================================================
+; benOS Bootloader - Print Stage
+; Copyright (C) 2018 Bench Computer, Inc. -- see ~/LICENSE
+;
+; Based on a free boot loader by E Dehling and the boot loading functions
+; found in Rust's Redox. Pieces from Ubuntu's boot loading functions
+; were also used in the benOS bootloader's library of parts as well as many
+; other bootloaders created over the years.
+; ==================================================================
+
+SECTION .text
+USE16
+; provide function for printing in x86 real mode
+
+; print a string and a newline
+; CLOBBER
+;   ax
+print_line:
+    mov al, 13
+    call print_char
+    mov al, 10
+    jmp print_char
+
+; print a string
+; IN
+;   si: points at zero-terminated String
+; CLOBBER
+;   si, ax
+print:
+    pushf
+    cld
+.loop:
+    lodsb
+    test al, al
+    jz .done
+    call print_char
+    jmp .loop
+.done:
+    popf
+    ret
+
+; print a character
+; IN
+;   al: character to print
+print_char:
+    pusha
+    mov bx, 7
+    mov ah, 0x0e
+    int 0x10
+    popa
+    ret
+
+; print a number in hex
+; IN
+;   bx: the number
+; CLOBBER
+;   al, cx
+print_hex:
+    mov cx, 4
+.lp:
+    mov al, bh
+    shr al, 4
+
+    cmp al, 0xA
+    jb .below_0xA
+
+    add al, 'A' - 0xA - '0'
+.below_0xA:
+    add al, '0'
+
+    call print_char
+
+    shl bx, 4
+    loop .lp
+
+    ret
